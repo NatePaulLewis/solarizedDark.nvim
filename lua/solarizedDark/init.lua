4c4d122180c484bac9a3096d38a5450ba9b74be8
Nate Lewis <nathanael.paul.lewis@gmail.com>
2023-05-23 14:55:24 -0500

    edits

--- lua/solarizedDark/init.lua
+++ lua/solarizedDark/init.lua
@@ -6,8 +6,8 @@ M.config = {
   underline = true,
   bold = true,
   italic = {
-    strings = true,
-    comments = true,
+    strings = false,
+    comments = false,
     operators = false,
     folds = true,
   },
